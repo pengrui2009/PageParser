ca9c2bf2acd1fba5e7de9a44e86bce5f8190c075
rui.peng <rui.peng@tusen.ai>
2023-06-10 23:23:45 +0800

    support fetch from html and store in json

--- jobs_handle_spider.py
+++ jobs_handle_spider.py
@@ -1,16 +1,26 @@
 
 # page_parser 包使用示例：6行代码写爬虫
 
+import json
 import requests
+import datetime
 from page_parser.weixin_parser import WeixinParser
 
 # 1、下载网页
 response = requests.get("https://mp.weixin.qq.com/s/fZp8gs-Uayckuia6WJ89ag")
 html = response.content.decode("utf-8")
-print('html:{}'.format(html))
+# print('html:{}'.format(html))
 # 2、解析网页
 items = WeixinParser().parse_index(html)
 
 # 3、输出数据
-for item in items: print(item)
+date_str = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+fileName = date_str + ".json"
+file = open(fileName, "w", encoding='utf-8')
+# json.dump(json_str, file)
+file.write(json.dumps(items, indent=4, ensure_ascii=False))
+file.close()
+
+# for item in items: 
+    # print(item)
 # {'title': '百度一下，你就知道'}
--- page_parser/weixin_parser.py
+++ page_parser/weixin_parser.py
@@ -1,10 +1,88 @@
 # -*- coding: utf-8 -*-
 
 # @Date    : 2018-10-13
-# @Author  : Peng Shiyu
+# @Author  : rui.peng
 
+
+import datetime
 from parsel import Selector
 
+class JobDescribe():
+    def __init__(self, title, content, require, datetime, salares, distance, address, 
+                 contract, comments, iconurl, userId, completed, createTime, completeTime, expired) -> None:
+        self._title = title
+        self._content = content
+        self._requirements = require
+        self._datetime = datetime
+        self._salares = salares
+        self._distance = distance
+        self._address = address
+        self._contract = contract
+        self._comments = comments
+        self._iconurl = iconurl
+        self._userId = userId
+        self._completed = completed
+        self._createTime = createTime
+        self._completeTime = completeTime
+        self._expired = expired
+
+
+    def GetJobElem(self) :
+        return {'': 0, '': 0, 'expired': self._expired}
+
+def parse_title(str):
+    result = None
+    if '招聘岗位' in str :
+        result = str        
+    return result
+
+def parse_datetime(str) :
+    result = None
+    if '工作时间' in str :
+        result = str        
+    return result
+
+def parse_content(str) :
+    result = None
+    if '工作内容' in str :
+        result = str        
+    return result
+
+def parse_salares(str) :
+    result = None
+    if '工资待遇' in str :
+        result = str        
+    return result
+
+def parse_distance(str) :
+    result = None
+    if '工作地点距离' in str :
+        result = str        
+    return result
+
+def parse_requirements(str) :
+    result = None
+    if '招聘人数' in str :
+        result = str        
+    return result
+
+def parse_address(str) :
+    result = None
+    if '工作地点' in str :
+        result = str        
+    return result
+
+def parse_contrace(str) :
+    result = None
+    if '报名加微信' in str or '报名微信' in str or '工作时间' in str or '微信' in str :
+        result = str        
+    return result
+
+def parse_comments(str) :
+    result = None
+    if '工作要求' in str :
+        result = str        
+    return result
 
 class WeixinParser(object):
     """
@@ -20,11 +98,100 @@ class WeixinParser(object):
         """
         sel = Selector(html)
         
-        title = sel.css("title::text").extract_first()
-        item = {
-            "title": title
-        }
-        yield item
+        # title = sel.css('//*[@id="js_content"]').extract_first()
+        # contents = sel.xpath('/html/body/div[1]/div[2]/div[1]/div/div[1]/div[3]').extract_first()
+        sections = sel.xpath('/html/body/div[1]/div[2]/div[1]/div/div[1]/div[3]/section')
+        # print(len(sections))
+        index = 1
+        items = []
+        current_datetime = datetime.datetime.now() + datetime.timedelta(days=1)
+        # current_datetime.datetime.timedelta(days=1)
+        current_datetime.replace(hour=23, minute=59, second=59)
+        expired_time = int((current_datetime - datetime.datetime(1970, 1, 1)).total_seconds())
+        datestr = current_datetime.strftime('%Y-%m-%d')
+        print('{} {}'.format(datestr, expired_time))
+        
+        for section in sections:
+            # print('section:{}'.format(section))
+            ps = section.xpath('//*[@id="js_content"]/section['+ str(index) + ']/section/section/p')
+            # print('len(ps):{}'.format(len(ps)))
+            index1 = 1
+            jobinfo = {'userId': {'$oid': '64537dc3e766bb008529e762'}, 'title': '', 'content': '', 'datetime': ''
+                     , 'salares': '', 'distance': '' , 'address': '', 'contract': '', 'comments': '', 
+                     'completed': 'false', 'expired': 0}
+            for p in ps :
+                p_text = p.xpath('//*[@id="js_content"]/section['+ str(index) + ']/section/section/p[' + str(index1) + ']/span/text()').extract_first()
+                print('{}'.format(p_text))
+                if p_text != None :
+                    title = parse_title(p_text)
+                    if title != None :
+                        jobinfo['title'] = title                    
+                    # parse content
+                    workcontent = parse_content(p_text)
+                    if workcontent != None :
+                        jobinfo['content'] = workcontent
+                    # parse requirements
+                    requirements = parse_requirements(p_text)
+                    if requirements != None :
+                        jobinfo['requirements'] = requirements
+                    # parse datetime
+                    worktime = parse_datetime(p_text)
+                    if worktime != None :
+                        jobinfo['datetime'] = worktime
+                    # parse salares
+                    worksalares = parse_salares(p_text)
+                    if worksalares != None :
+                        jobinfo['salares'] = worksalares
+                    # parse distance
+                    workdistance = parse_distance(p_text)
+                    if workdistance != None :
+                        jobinfo['distance'] = workdistance
+                    # parse address
+                    workaddress = parse_address(p_text)
+                    if workaddress != None :
+                        jobinfo['address'] = workaddress
+                    # parse contract
+                    workcontract = parse_contrace(p_text)
+                    if workcontract != None :
+                        jobinfo['contract'] = workcontract
+                    # parse comments
+                    workcomments = parse_contrace(p_text)
+                    if workcomments != None :
+                        jobinfo['comments'] = workcomments                    
+
+                index1 = index1 + 1
+            jobinfo['expired'] = expired_time
+            items.append(jobinfo)
+            index = index+1
+            print('**********************************')
+        return items            
+        # print(type(contents))
+        
+        # for content in contents :
+        #     print('{}'.format(content))
+        # for i in range(len(contents)) :
+        #     print('+++++++++++++++++++++')
+        #     xpath_str = '//*[@id="js_content"]/section['+ str(i) + ']/section/section/p'
+        #     # print('xpath:{}'.format(xpath_str))
+        #     section_p = contents.xpath(xpath_str)
+            
+        #     # section_num_str = '//*[@id="js_content"]/section['+ str(i) + ']/section/section/p'
+        #     # section_num = len(section.xpath(section_num_str))
+        #     # print('section_num:{}'.format(section_num))
+        #     # print('*********************, len:{}'.format(len(section)))
+        #     for index in range(len(section_p)) :
+        #         p_xpath_str = '//*[@id="js_content"]/section['+ str(i) + ']/section/section/p[' + str(index) + ']/span/text()'
+        #         p = section.xpath(p_xpath_str).extract_first()
+        #         print('p:{}'.format(p))
+        #         # if p == None:
+        #         #     break
+            
+        #     # print('section:{}'.format(section))
+        #     # if section == None :
+        #         # print('section is none')
+        #         # break
+
+        # yield item
 
 
 if __name__ == '__main__':
